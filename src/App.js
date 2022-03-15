9ef4d00efa05361bf2ba813c79566054b759d3bb
Jetigeri <szabo.gergo87j@gmail.com>
2022-03-15 20:21:27 +0100

    1/3 widgets finished: accordion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Accordion from "./components/Accordion";
+
+const items = [
+  {
+    title: "What is react",
+    content: "React is a front end js framework",
+  },
+  {
+    title: "Why use react",
+    content: "React is a fav js library among engineers",
+  },
+  {
+    title: "How do you use react",
+    content: "You just simply create components and thats it :)",
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Accordion items={items}></Accordion>
     </div>
   );
 }
